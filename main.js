148a90376c30471f07f7ac087849a9ace5257eca
RediiChen <a0929052958@gmail.com>
2021-03-10 19:38:50 +0800

    建立基本下一張和點擊切換功能

--- main.js
+++ main.js
@@ -1,27 +1,51 @@
-var s_i = 1;//Sliders index
+// Part 1 : 變數宣告區 =========================
+var i_s = 0;//index of sliders, from 0
 var prevBtn = document.querySelector("#btn-prev"),
     nextBtn = document.querySelector("#btn-next");
+var item = document.getElementsByClassName("slider"),
+    dots = document.getElementsByClassName("dots");
+//取得是擁有相同類別名稱的一整組-->陣列
 
-    
-//點擊後要做的事情
-nextBtn.onclick = next ;
-prevBtn.onclick = prev ;
+// Part 2 : 應對設定區 ========== 點擊後要做的事情
+nextBtn.onclick = next;
+prevBtn.onclick = prev;
 //! 不可以加函數的小括弧
+for (let i = 0 ; i < dots.length ; i++) {
+    // dots[i].onclick = function () {
+    //     clickSlider(i) ;
+    // }
+    dots[i].onclick = clickSlider(i) ;
+    //要放參數的函數就要另外用function格式去寫，
+    // 否則會無法讀取
+}
+// Part 3 : 馬上執行區 ==========================
 
-// 函數宣告區 ==========================
-function next() {
-    s_i++ ;
-    if (s_i >= 3) {
-        s_i %= 3 ;
+showSlider();
+
+// Part 4 : 函數宣告區 ==========================
+function showSlider() {
+    //通通解除active的狀態
+    for (let i = 0; i < item.length; i++) {
+        //項目
+        item[i].classList.remove("slider-active");
+        //點點
+        dots[i].classList.remove("dots-active");
     }
-    console.log(s_i) ;
+    //啟動指定編號的狀態
+    item[i_s].classList.add("slider-active");
+    dots[i_s].classList.add("dots-active");
+}
+function clickSlider(n) {
+    i_s = n ;
+    showSlider() ;
+}
+function next() {
+    i_s++;
+    if (i_s == item.length) i_s = 0;//額滿就歸零
+    showSlider();//顯示此時的編號
 }
 function prev() {
-    s_i-- ;
-    if (s_i < 0) {
-        s_i += 3 ;
-        //! JS 的%在負數的計算會不符合數學上的餘數
-        // 補救方法亦有： ((x&n) + n)
-    }
-    console.log(s_i) ;
+    i_s--;
+    if (i_s == -1) i_s += item.length;
+    showSlider();//顯示此時的編號
 }
