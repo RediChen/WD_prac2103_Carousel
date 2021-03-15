15f212eb17090e1dc15662719e299e0c915c3f78
RediiChen <a0929052958@gmail.com>
2021-03-15 17:06:14 +0800

    JS程式碼排版微調

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // Part 1 : 變數宣告區 =========================
 var i_s = 0;//index of sliders, from 0
-var box = document.getElementById("slider-box") ;
+var box = document.getElementById("slider-box");
 var prevBtn = document.querySelector("#btn-prev"),
     nextBtn = document.querySelector("#btn-next");
 var item = document.getElementsByClassName("slider"),
     dots = document.getElementsByClassName("dots");//取得是擁有相同類別名稱的一整組-->陣列
 var interval, userInterval = box.getAttribute("data-crs-interval");//自動播放的預設時間間隔
-var timer = setInterval(autoPlay, interval) ;
+var timer = setInterval(autoPlay, interval);
 // 使用JS 內建的API：將函數以指定頻率重複執行
 // 存成變數以備用。
-var ifShowDots = box.getAttribute("data-crs-showdots") ;
-console.log("ifShowDots = " + ifShowDots) ;
+var ifShowDots = box.getAttribute("data-crs-showdots");
+console.log("ifShowDots = " + ifShowDots);
 
 // Part 2 : 馬上執行區 ==========================
-if(ifShowDots == "false") {//!都須字串賦值
-    var dotBox = document.getElementById("dot-box") ;
-    dotBox.style["display"] = "none" ;
+if (ifShowDots == "false") {//!都須字串賦值
+    var dotBox = document.getElementById("dot-box");
+    dotBox.style["display"] = "none";
 }
 showSlider();
 
@@ -25,14 +25,14 @@ prevBtn.onclick = prev;
 //! 不可以加函數的小括弧
 for (let i = 0; i < dots.length; i++) {
     dots[i].onclick = function () {
-        clickDot(i) ;
+        clickDot(i);
     }
     // dots[i].onclick = clickDot(i);
     //要放參數的函數就要另外用function格式去寫，
     // 否則會無法讀取
 }
-if (userInterval) interval = userInterval ;
-else interval = 3000 ;//自動播放的預設時間間隔
+if (userInterval) interval = userInterval;
+else interval = 3000;//自動播放的預設時間間隔
 
 // Part 4 : 函數宣告區 ==========================
 function showSlider() {
@@ -50,19 +50,19 @@ function showSlider() {
 function clickDot(n) {
     i_s = n;
     showSlider();
-    resetTimer() ;
+    resetTimer();
 }
 function next() {
     i_s++;
     if (i_s == item.length) i_s = 0;//額滿就歸零
     showSlider();//顯示此時的編號
-    resetTimer() ;
+    resetTimer();
 }
 function prev() {
     i_s--;
     if (i_s == -1) i_s += item.length;
     showSlider();//顯示此時的編號
-    resetTimer() ;
+    resetTimer();
 }
 
 // 自動播放功能
@@ -73,6 +73,6 @@ function autoPlay() {
 //實務需求：須配合按鈕的點擊
 function resetTimer() {
     //重置計時器 = 還原＋重啟
-    clearInterval(timer) ;
-    timer = setInterval(autoPlay, interval) ;
+    clearInterval(timer);
+    timer = setInterval(autoPlay, interval);
 }
